3d360678ee9e6468604fe992dfc0910c96b2336e
Hao Chen <mmkrty@gmail.com>
2022-12-28 21:06:17 +0100

    add schema for technology and project, add navbar

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,15 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import blockContent from "./blockContent";
+import category from "./category";
+import post from "./post";
+import author from "./author";
+import technology from "./technology";
+import project from "./project";
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  technology,
+  project,
+];
---
+++ schemas/project.js
@@ -0,0 +1,43 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
+      name: "link",
+      title: "Link",
+      type: "string",
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: "technologies",
+      title: "Technologies",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "technology" }],
+        },
+      ],
+    }),
+  ],
+});
