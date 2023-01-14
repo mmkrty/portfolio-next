b4721b05c7c6ca39f76a3973d5792eba3e3da686
Hao Chen <mmkrty@gmail.com>
2023-01-14 13:08:03 +0100

    add more icons in public, adjust skills section

--- components/skills/skills.jsx
+++ components/skills/skills.jsx
@@ -10,12 +10,47 @@ function Skills() {
       <div className="flex flex-col justify-center mx-6 xs:mx-14">
         <SectionHeading title="Skills" />
         <p>
-          In addition to HTML5, CSS3, and Javascript ES6+, here are some of the
-          techs I worked with recently:
+          I have explored various technologies in the field of web development,
+          but I have chosen to delve deeper into a select few, including:
         </p>
         <div className="font-fira mt-3 gap-4 grid grid-cols-1 md:grid-cols-2">
           <div className="bg-primary_black_light py-3 px-4 rounded-sm">
             <h3 className="text-xl font-semibold">Frontend</h3>
+            <div className="flex justify-around items-end my-6 gap-2">
+              <div className="flex flex-col justify-center items-center">
+                <img
+                  src="/icons/html5_icon.svg"
+                  alt="HTML5"
+                  className="filter-grey"
+                  width="48px"
+                />
+                <h4 className="mt-1">HTML5</h4>
+              </div>
+
+              <div className="flex flex-col justify-center items-center">
+                <img
+                  src="/icons/css3_icon.svg"
+                  alt="CSS3"
+                  className="filter-grey"
+                  width="52px"
+                />
+                <h4 className="mt-1">CSS3</h4>
+              </div>
+
+              <div className="flex flex-col justify-center items-center">
+                <img
+                  src="/icons/javascript_icon.svg"
+                  alt="Javascript"
+                  className="filter-grey"
+                  width="48px"
+                />
+                <h4 className="mt-1">Javascript</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-primary_black_light py-3 px-4 rounded-sm">
+            <h3 className="text-xl font-semibold">Frameworks/Libraries</h3>
             <div className="flex justify-around items-end my-6 gap-2">
               <div className="flex flex-col justify-center items-center">
                 <img
@@ -48,7 +83,7 @@ function Skills() {
           </div>
 
           <div className="bg-primary_black_light py-3 px-4 rounded-sm">
-            <h3 className="text-xl font-semibold ">Backend & BaaS</h3>
+            <h3 className="text-xl font-semibold ">Backend & Database</h3>
             <div className="flex justify-around items-end my-6 gap-2 ">
               <div className="flex flex-col justify-center items-center">
                 <img
@@ -59,30 +94,7 @@ function Skills() {
                 />
                 <h4 className="mt-1">Rails</h4>
               </div>
-              <div className="flex flex-col justify-center items-center">
-                <img
-                  src="/icons/node_js_icon.svg"
-                  alt="nodejs"
-                  className="filter-grey"
-                  width="48px"
-                />
-                <h4 className="mt-1">NodeJS</h4>
-              </div>
-              <div className="flex flex-col justify-center items-center">
-                <img
-                  src="/icons/logo_firebase_icon.svg"
-                  alt="firebase"
-                  className="filter-grey"
-                  width="48px"
-                />
-                <h4 className="mt-1">Firebase</h4>
-              </div>
-            </div>
-          </div>
 
-          <div className="bg-primary_black_light py-3 px-4 rounded-sm">
-            <h3 className="text-xl font-semibold">Database</h3>
-            <div className="flex justify-around items-end my-6 gap-2">
               <div className="flex flex-col justify-center items-center">
                 <img
                   src="/icons/postgresql_icon.svg"
@@ -92,14 +104,15 @@ function Skills() {
                 />
                 <h4 className="mt-1">PostgreSQL</h4>
               </div>
+
               <div className="flex flex-col justify-center items-center">
                 <img
-                  src="/icons/mongodb_icon.svg"
-                  alt="mongodb"
+                  src="/icons/logo_firebase_icon.svg"
+                  alt="firebase"
                   className="filter-grey"
                   width="48px"
                 />
-                <h4 className="mt-1">MongoDb</h4>
+                <h4 className="mt-1">Firebase</h4>
               </div>
             </div>
           </div>
