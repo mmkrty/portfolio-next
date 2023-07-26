a727b8a5c8168df8a8a92c8d830221c81677e427
Hao Chen <mmkrty@gmail.com>
2023-07-26 17:07:34 +0200

    adjust contact section

--- components/contact/contact.jsx
+++ components/contact/contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SectionHeading from "../sectionHeading/sectionHeading";
 import LinkButton from "../linkButton/linkButton";
 
-function Contact() {
+function Contact({ contacttext, email }) {
   return (
     <section
       id="contact"
@@ -12,12 +12,9 @@ function Contact() {
         <SectionHeading title="Contact" />
 
         <p className="max-w-xl text-justify mb-10">
-          I am currently actively searching for new opportunities. If you have a
-          project or opportunity that aligns with my skillset, or if you are
-          interested in collaborating on a project, please don't hesitate to
-          reach out to me. I look forward to hearing from you.
+          {contacttext}
         </p>
-        <LinkButton href="mailto:liuhaoch1990@gmail.com" content="Let's Chat" />
+        <LinkButton href={email} content="Let's Chat" />
       </div>
     </section>
   );
